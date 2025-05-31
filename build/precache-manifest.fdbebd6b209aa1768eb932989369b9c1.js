178654fed8dc24fdaab8e2c17f9dd0971ba0d1d6
Er-MayankRajput <mnkrjpt408@gmail.com>
2025-05-31 21:36:19 +0530

    update latest version

---
+++ build/precache-manifest.fdbebd6b209aa1768eb932989369b9c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c681fa4a0564a1e1523f42c44ce5a4db",
+    "url": "/Er-MayankRajput/Robogame/index.html"
+  },
+  {
+    "revision": "fae138e148c182550a80",
+    "url": "/Er-MayankRajput/Robogame/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "3d75bd0ffa47a9b0d77d",
+    "url": "/Er-MayankRajput/Robogame/static/css/main.c64b3eb0.chunk.css"
+  },
+  {
+    "revision": "fae138e148c182550a80",
+    "url": "/Er-MayankRajput/Robogame/static/js/2.fa8448ff.chunk.js"
+  },
+  {
+    "revision": "3d75bd0ffa47a9b0d77d",
+    "url": "/Er-MayankRajput/Robogame/static/js/main.929bbd22.chunk.js"
+  },
+  {
+    "revision": "56844e8656228d804edc",
+    "url": "/Er-MayankRajput/Robogame/static/js/runtime~main.3b41284d.js"
+  }
+]);
