5622c97d3cecc1b86ca01141e552a5c55168b299
Er-MayankRajput <mnkrjpt408@gmail.com>
2025-05-31 22:46:27 +0530

    update packag3e

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React , {Component} from "react";
+import React , {useState ,useEffect} from "react";
 import CardList from '../components/Cardlist';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
@@ -13,27 +13,40 @@ const state ={
     searchfield:""
 }
 
-class App extends Component {
-    constructor(){
-        super()
-        this.state={
-            Robots:[],
-            searchfield:"",
-        }
-    }
+function App() {
+    //constructor(){
+     //  super()
+        //this.state={
+          //  Robots:[],
+            //searchfield:"",
+        //}
+    //}
 
-    componentDidMount(){
+    const [Robots ,setRobots] =useState([])
+    const [searchfield ,setsearchfield]=useState('')
+    const [count ,setcount]=useState(0)
+
+
+    //componentDidMount(){
+      //  fetch('https://jsonplaceholder.typicode.com/users')
+     //   .then(Response=> Response.json())
+    //  .then(users =>this.setState({Robots:users}));        
+    //}
+
+    useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(Response=> Response.json())
-        .then(users =>this.setState({Robots:users}));
-        
-    }
-    onSearchange=(event)=>{
-        this.setState({searchfield : event.target.value})
-        }
+        //.then(users =>this.setState({Robots:users}));
+        .then(users =>setRobots(users));
+        console.log(count)
+    },[count])
 
-    render() {
-        const {Robots ,searchfield}=this.state
+
+    const onSearchange=(event)=>{
+        //this.setState({searchfield : event.target.value})
+        setsearchfield(event.target.value)
+        }
+        //const {Robots ,searchfield}=this.state
         const filterRobots=Robots.filter(Robot=>{
             return Robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
@@ -41,7 +54,8 @@ class App extends Component {
        (
         <div className="tc">
         <h1>Robo Friends</h1> 
-        <SearchBox searchChange={this.onSearchange}/>
+        <button onclick={()=>setCount(setcount+1)}>Click Me </button>
+        <SearchBox searchChange={onSearchange}/>
         <Scroll>
             <ErrorBoundry >
             <CardList Robot={filterRobots}/>
@@ -50,7 +64,7 @@ class App extends Component {
         </div>
     );
 }
- }
+ 
 
 
 export default App;
